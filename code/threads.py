660fe7ac2d436b0198a8b63d1676b1267703b3cf
unknown <vpsuri@irusmail.com>
2020-08-05 18:38:26 -0600

    add threads

---
+++ code/threads.py
@@ -0,0 +1,18 @@
+import threading
+import time
+
+def multiply(multiplicand, multiplier):
+    count=1
+    while count <= 10:
+        time.sleep(.5)
+        multiplicand=multiplier*multiplicand
+        print(multiplicand)
+        count=count+1
+
+try:
+    x = threading.Thread(target=multiply, args=(3,3))
+    x.start()
+    y = threading.Thread(target=multiply, args=(10,2))
+    y.start()
+except:
+    Exception,e: print str(e)
