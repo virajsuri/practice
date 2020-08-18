373a6326a5993fea5716c26e2e74213d462704b4
unknown <vpsuri@irusmail.com>
2020-08-18 17:48:24 -0600

    changing getStockPrice, moving bloomberg to new repo

--- getStockPrice.py
+++ getStockPrice.py
@@ -1,11 +1,38 @@
-import openpyxl as xl
 import yfinance as yf
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 def getStockPrice(name):
+    #returns O/H/L/C/Volume/Divs/Stock Splits
+
     stock = yf.Ticker(name)
-    closingPrice = stock.history()
-    print(name)
-    print(closingPrice['Close'][len(closingPrice)-1])
+    closingPrice = stock.history(period="ytd")
+    # print(name)
+    # print(closingPrice['Close'])
+    # print(closingPrice['Close'][len(closingPrice)-1])
+    return closingPrice
+
+def plotGraph(outputTable, field):
+    print("Plotting graph")
+    fig, (ax1, ax2) = plt.subplots(2)
+    
+    ax1.plot(outputTable[field], color='black',linestyle='solid')
+    ax2.plot(outputTable['Volume'], color='red',linestyle='solid')
+
+    scale_y = 1e6
+    ax2.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y)))
+
+    ax1.set_title(field)
+    ax2.set_title('Volume')    
+
+    ax2.set_ylabel('Volume in millions')
+
+    plt.tight_layout()
+    # plt.savefig(outputTable.columns[0]+".jpg", dpi=300)
+    plt.show()
 
 
-getStockPrice("MSFT")
+field = 'Close'
+ticker = 'MSFT'
+stockInfo = getStockPrice(ticker) 
+plotGraph(stockInfo, field)
